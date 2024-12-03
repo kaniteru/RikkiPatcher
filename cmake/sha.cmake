143c6c0031e1499742b767c90a72f2432a4583f7
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2024-12-03 21:57:46 +0900

    changed repo url

--- cmake/sha.cmake
+++ cmake/sha.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
         sha
-        GIT_REPOSITORY https://github.com/pr0f3ss/SHA.git
+        GIT_REPOSITORY https://github.com/ZeunO8/SHA.git
         GIT_TAG                 master
 )
 
