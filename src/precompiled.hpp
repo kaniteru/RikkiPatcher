6c8e28c47163631006f5161222166235097bbcfb
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2024-12-23 00:35:31 +0900

    edited some empty line

--- src/precompiled.hpp
+++ src/precompiled.hpp
@@ -1,9 +1,9 @@
 #ifndef RIKKI_PATCHER_PRECOMPILED_HPP
 #define RIKKI_PATCHER_PRECOMPILED_HPP
-
 #include <webview.h>
 #include <nlohmann/json.hpp>
 #include <SHA512.hpp>
+
 
 #include <windows.h>
 #include <shellapi.h>
@@ -28,6 +28,7 @@
 #include <iostream>
 #include <sstream>
 
+#include <exception>
 #include <typeindex>
 #include <filesystem>
 #include <functional>
