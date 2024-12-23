29e172e87984aa45946160b627bdb7609c214b92
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2024-12-23 23:10:14 +0900

    added 7zip

---
+++ cmake/7zip.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+
+function(download_7zip ver)
+    set(CONT_NAME "7zip_${ver}")
+    set(DOWNLOAD_URL "https://www.7-zip.org/a/7z${ver}-extra.7z")
+
+    FetchContent_Declare(
+            ${CONT_NAME}
+            URL "${DOWNLOAD_URL}"
+    )
+
+    message("fetching ${CONT_NAME}")
+    FetchContent_MakeAvailable(${CONT_NAME})
+
+    set(${CONT_NAME}_DIR "${${CONT_NAME}_SOURCE_DIR}" PARENT_SCOPE)
+endfunction()
