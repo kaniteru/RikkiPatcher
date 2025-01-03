b12a360df85b850e714649c10007020af42bce54
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2025-01-03 12:48:48 +0900

    updated version

--- res/scripts/index.js
+++ res/scripts/index.js
@@ -1,4 +1,4 @@
-const RIKKI_PATCHER_VER = 241228;
+const RIKKI_PATCHER_VER = 250103;
 
 function onLoad() {
     Logger.INSTANCE.log(eLogLv.LOG_LV_INFO, `Rikki Patcher Version: ${RIKKI_PATCHER_VER}`);
