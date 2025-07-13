badc1718afc6c0dcbe585a2801ee02ee88a93946
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2025-07-14 00:18:22 +0900

    upgrade version
    Some users can't execute the program or showing only empty screen.
    I don't have a good idea for that, so I updated it to recent commit version.

--- cmake/webview.cmake
+++ cmake/webview.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         webview
         GIT_REPOSITORY https://github.com/webview/webview
-        GIT_TAG                 0.12.0
+        GIT_TAG        f9cbb69abe5d875bb7c4bafe8329340462ad1ca9
 )
 
 message(STATUS "fetching webview")
